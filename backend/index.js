1292cc8e06d318369b2e26d9429d1dfdc482e4a3
Shreyansh Gupta <2k22.cse.2212530@gmail.com>
2025-07-06 12:46:06 +0530

    update axios

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 app.use(cookieParser())
 app.use(
     cors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173", "https://chat-jgdpfxlll-shreyansh-gupta-s-projects.vercel.app/login"],
     credentials : true,
 })
 )
--- frontend/src/Lib/axios.js
+++ frontend/src/Lib/axios.js
@@ -1,7 +1,20 @@
+// // frontend/src/Lib/axios.js
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//   baseURL: "http://localhost:5002/api", // or your API URL
+//   withCredentials: true, // if you're using cookies for auth
+// });
+
 // frontend/src/Lib/axios.js
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5002/api"
+    : "https://chat-app-in3m.onrender.com/api"; // your Render backend
+
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5002/api", // or your API URL
-  withCredentials: true, // if you're using cookies for auth
+  baseURL: BASE_URL,
+  withCredentials: true, // required for sending cookies/JWTs
 });
