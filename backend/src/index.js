a19b1ffd9d8bd5164d353bc698d66d5e60094b6a
Shreyansh Gupta <2k22.cse.2212530@gmail.com>
2025-10-21 11:59:07 +0530

    change in index,js

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.VITE_FRONTEND_URL,
     credentials: true,
   })
 );
